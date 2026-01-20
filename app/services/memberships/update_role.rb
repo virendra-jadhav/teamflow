8f32a7083f8c9b3cb0f5fc65515b27e62a894078
Virendra Jadhav <virendrajadhav.dev@gmai.com>
2026-01-20 23:13:04 +0530

    TDD: Update and delete Membership on TDD approachach

--- app/services/memberships/update_role.rb
+++ app/services/memberships/update_role.rb
@@ -2,13 +2,22 @@ module Memberships
   class UpdateRole
     VALID_ROLES = %w[admin member].freeze
 
-    def initialize(membership:, role:)
+    def initialize(membership:, role:, actor:)
       @membership = membership
       @role = role
+      @actor = actor
     end
 
-    def call
+    def call(current_user:)
       return false unless VALID_ROLES.include?(@role)
+
+      # prevent admin demoting themselves
+      if @membership.user == @actor &&
+         @membership.admin? &&
+         @role == "member"
+        return false
+      end
+
       @membership.update(role: @role)
     end
   end
---
+++ spec/system/memberships/role_update_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe "Membership role update", type: :system do
+  let!(:account) { create(:account) }
+  let!(:admin) { create(:user, password: "password") }
+  let!(:member) { create(:user, password: "password") }
+
+  let!(:admin_membership) do
+    create(:membership, :admin, user: admin, account: account)
+  end
+  let!(:member_membership) do
+    create(:membership, user: member, account: account)
+  end
+
+  context "as admin" do
+    before do
+      login_as(admin)
+      visit account_switch_path(account_id: account.id)
+      visit accounts_settings_path
+    end
+
+    it "allows admin to promote a member" do
+      expect {
+        click_button "Make Admin"
+      }.to change {
+        member_membership.reload.role
+      }.from("member").to("admin")
+      expect(page).to have_content("Role updated successfully!")
+    end
+    it "does not allow admin to demote themselves" do
+      expect(page).not_to have_button("Make Member", exact: false)
+    end
+  end
+  context "as member" do
+    before do
+      login_as(member)
+      visit account_switch_path(account_id: account.id)
+      visit accounts_settings_path
+    end
+
+    it "does not show role update controls" do
+      expect(page).not_to have_button("Make Admin")
+      expect(page).not_to have_button("Make Member")
+    end
+  end
+end
